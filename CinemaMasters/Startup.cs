d374e5073c57e35fa0ff3d94b97e3778044308ce
zmartl-bbc <luca.marti@bluewin.ch>
2016-05-31 10:30:45 +0200

    Projektdateien hinzufügen.

---
+++ CinemaMasters/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CinemaMasters.Startup))]
+namespace CinemaMasters
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
